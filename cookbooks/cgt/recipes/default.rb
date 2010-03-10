1ea2915bc7263a0ea7d384e25872d0a72da737aa
bricooke <brian@madebyrocket.com>
2010-03-09 20:44:30 -0700

    rubyinline take 7

--- cookbooks/cgt/recipes/default.rb
+++ cookbooks/cgt/recipes/default.rb
@@ -3,9 +3,13 @@
 # Recipe:: default
 #
 execute "set ruby-inline permissions" do
-  command "find /data/caregivingtouchweb/releases/ -name ruby_inline -exec chmod 700 {} \;"
+  command %Q{
+    find /data/caregivingtouchweb/releases/ -name ruby_inline -exec chmod 700 {} \;
+  }
 end
 
 execute "set .ruby-inline permissions" do
-  command "find /data/caregivingtouchweb/releases/ -name .ruby_inline -exec chmod 700 {} \;"
+  command %Q{
+    find /data/caregivingtouchweb/releases/ -name .ruby_inline -exec chmod 700 {} \;
+  }
 end
